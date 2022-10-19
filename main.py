828c0900e3e5c4b56bc31e2e3f2c7a62f483c0f9
Sharik-oss <62305699+Sharik-oss@users.noreply.github.com>
2022-10-19 21:22:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 import sys
 import random
  
+from player import Player
 pygame.init()
 vec = pygame.math.Vector2 #2 for two dimensional
  
@@ -20,58 +21,7 @@ pygame.display.set_caption("Game")
 bg = pygame.image.load('Background.png')
 bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
  
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__() 
-        self.image = pygame.image.load("Player.png")
-        self.rect = self.image.get_rect()
-        self.pos = vec((10, 360))
-        self.vel = vec(0,0)
-        self.jumping = False
-        self.acc = vec(0,0)
-        self.player_level = 0
- 
-    def move(self):
-        self.acc = vec(0,0.5)
-    
-        pressed_keys = pygame.key.get_pressed()
-                
-        if pressed_keys[K_LEFT]:
-            self.acc.x = -ACC
-        if pressed_keys[K_RIGHT]:
-            self.acc.x = ACC
-                 
-        self.acc.x += self.vel.x * FRIC
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-         
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
-             
-        self.rect.midbottom = self.pos
- 
-    def jump(self):
-        self.rect.y += 2
-        hits = pygame.sprite.spritecollide(self, platforms, False)
-        self.rect.y -= 2
-        if hits:
-            self.jumping = True
-            self.vel.y = -15
- 
- 
-    def update(self):
-        hits = pygame.sprite.spritecollide(P1 ,platforms, False)
-        if P1.vel.y > 0:        
-            if hits:
-                self.vel.y = 0
-                self.pos.y = hits[0].rect.top + 10
-                self.player_level += 1
-                
-    def is_colided_with(self, sprite):
-        return self.rect.colliderect(sprite.rect)
- 
+
  
 class platform(pygame.sprite.Sprite):
     def __init__(self):
